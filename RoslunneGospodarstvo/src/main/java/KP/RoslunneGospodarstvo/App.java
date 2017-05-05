b38980ca7360cc3f6e1796bbc4cc5fcc6ec814c3
Zakk <tapac.zakusylo@gmail.com>
2017-05-05 18:54:24 +0300

    Start

---
+++ RoslunneGospodarstvo/src/main/java/KP/RoslunneGospodarstvo/App.java
@@ -0,0 +1,13 @@
+package KP.RoslunneGospodarstvo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
