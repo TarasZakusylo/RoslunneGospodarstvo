1b79a228d7ff368a7efdcd8d81882c3d58bf1cc2
Zakk <tapac.zakusylo@gmail.com>
2017-05-08 13:29:44 +0300

    Закінчив із дизайном перших питань кімнатних рослин

--- RoslunneGospodarstvo/src/main/java/KP/RoslunneGospodarstvo/Kimnatne.java
+++ RoslunneGospodarstvo/src/main/java/KP/RoslunneGospodarstvo/Kimnatne.java
@@ -48,7 +48,6 @@ public class Kimnatne extends JFrame {
 	private ButtonGroup group_putanna8; 
 	private ButtonGroup group_putanna3; 
 	private JRadioButton radioB_putanna6_2;
-
 	private JButton b_Dali;
 
 	SliderListener4 slider4 = new SliderListener4();
@@ -73,6 +72,7 @@ public class Kimnatne extends JFrame {
 	private JRadioButton radioB_putanna3_1;
 	private JRadioButton radioB_putanna3_2;
 	private JLabel l_slider_putanna5_3;
+	private JButton b_putanna2;
 
 	public Kimnatne(String s) {
 		super(s);
@@ -93,56 +93,64 @@ public class Kimnatne extends JFrame {
 		getContentPane().setLayout(null);
 		getContentPane().add(b_Nazad);
 
-		l_Hapka = new JLabel("Пройдіть, будь ласка, опитування");
-		l_Hapka.setForeground(Color.ORANGE);
+		l_Hapka = new JLabel("Будь ласка, пройдіть, , опитування");
+		l_Hapka.setForeground(Color.YELLOW);
 		l_Hapka.setFont(new Font("Palatino Linotype", Font.BOLD | Font.ITALIC, 25));
 		l_Hapka.setHorizontalAlignment(SwingConstants.CENTER);
 		l_Hapka.setBounds(90, 4, 595, 30);
 		getContentPane().add(l_Hapka);
 
 		l_putanna1 = new JLabel("У Вас зимовий сад, оранжерея ?");
+		l_putanna1.setForeground(Color.WHITE);
 		l_putanna1.setHorizontalAlignment(SwingConstants.CENTER);
 		l_putanna1.setFont(new Font("Times New Roman", Font.BOLD, 20));
 		l_putanna1.setBounds(8, 47, 501, 30);
 		getContentPane().add(l_putanna1);
 
 		l_putanna2 = new JLabel("Вам цікавий бонсай чи суісекі ?");
+		l_putanna2.setForeground(Color.WHITE);
 		l_putanna2.setHorizontalAlignment(SwingConstants.CENTER);
 		l_putanna2.setFont(new Font("Times New Roman", Font.BOLD, 20));
 		l_putanna2.setBounds(8, 110, 501, 30);
 		getContentPane().add(l_putanna2);
 
 		l_putanna4 = new JLabel("Наскільки Ви забобонні ? Вас цікавлять прикмети ?");
+		l_putanna4.setForeground(Color.WHITE);
 		l_putanna4.setHorizontalAlignment(SwingConstants.CENTER);
 		l_putanna4.setFont(new Font("Times New Roman", Font.BOLD, 20));
 		l_putanna4.setBounds(8, 234, 501, 30);
 		getContentPane().add(l_putanna4);
 
 		l_putanna3 = new JLabel("Вас цікавить купівля кімнатних рослин ?");
+		l_putanna3.setForeground(Color.WHITE);
 		l_putanna3.setHorizontalAlignment(SwingConstants.CENTER);
 		l_putanna3.setFont(new Font("Times New Roman", Font.BOLD, 20));
 		l_putanna3.setBounds(8, 174, 501, 30);
 		getContentPane().add(l_putanna3);
 
 		l_putanna6 = new JLabel("Ви цікавитесь шкідниками кімнатних рослин?");
+		l_putanna6.setForeground(Color.WHITE);
 		l_putanna6.setHorizontalAlignment(SwingConstants.CENTER);
 		l_putanna6.setFont(new Font("Times New Roman", Font.BOLD, 20));
 		l_putanna6.setBounds(8, 363, 501, 30);
 		getContentPane().add(l_putanna6);
 
 		l_putanna5 = new JLabel("Як часто Ви доглядатимете за рослинами ?");
+		l_putanna5.setForeground(Color.WHITE);
 		l_putanna5.setHorizontalAlignment(SwingConstants.CENTER);
 		l_putanna5.setFont(new Font("Times New Roman", Font.BOLD, 20));
 		l_putanna5.setBounds(12, 298, 501, 30);
 		getContentPane().add(l_putanna5);
 
 		l_putanna7 = new JLabel("Вас цікаві хвороби кімнатних рослин ?");
+		l_putanna7.setForeground(Color.WHITE);
 		l_putanna7.setHorizontalAlignment(SwingConstants.CENTER);
 		l_putanna7.setFont(new Font("Times New Roman", Font.BOLD, 20));
 		l_putanna7.setBounds(8, 421, 501, 30);
 		getContentPane().add(l_putanna7);
 
 		radioB_putanna1_1 = new JRadioButton("Так");
+		radioB_putanna1_1.setForeground(Color.WHITE);
 		radioB_putanna1_1.setOpaque(false);
 		radioB_putanna1_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -154,6 +162,7 @@ public class Kimnatne extends JFrame {
 		getContentPane().add(radioB_putanna1_1);
 
 		radioB_putanna1_2 = new JRadioButton("Ні");
+		radioB_putanna1_2.setForeground(Color.WHITE);
 		radioB_putanna1_2.setOpaque(false);
 		radioB_putanna1_2.setFont(new Font("Times New Roman", Font.ITALIC, 18));
 		radioB_putanna1_2.setBounds(672, 51, 83, 25);
@@ -165,7 +174,7 @@ public class Kimnatne extends JFrame {
 		});
 
 		radioB_putanna2_1 = new JRadioButton("Так");
-		radioB_putanna2_1.setForeground(Color.BLACK);
+		radioB_putanna2_1.setForeground(Color.WHITE);
 		radioB_putanna2_1.setOpaque(false);
 		radioB_putanna2_1.setFont(new Font("Times New Roman", Font.ITALIC, 18));
 		radioB_putanna2_1.setBounds(523, 115, 62, 25);
@@ -177,7 +186,7 @@ public class Kimnatne extends JFrame {
 		});
 
 		radioB_putanna2_2 = new JRadioButton("Ні");
-		radioB_putanna2_2.setForeground(Color.BLACK);
+		radioB_putanna2_2.setForeground(Color.WHITE);
 		radioB_putanna2_2.setOpaque(false);
 		radioB_putanna2_2.setFont(new Font("Times New Roman", Font.ITALIC, 18));
 		radioB_putanna2_2.setBounds(600, 114, 55, 25);
@@ -194,13 +203,13 @@ public class Kimnatne extends JFrame {
 		getContentPane().add(slider_putanna4);
 
 		l_slider_putanna4_1 = new JLabel("Зовсім");
-		l_slider_putanna4_1.setForeground(Color.BLACK);
+		l_slider_putanna4_1.setForeground(Color.WHITE);
 		l_slider_putanna4_1.setFont(new Font("Times New Roman", Font.ITALIC, 18));
 		l_slider_putanna4_1.setBounds(521, 261, 115, 16);
 		getContentPane().add(l_slider_putanna4_1);
 
 		l_slider_putanna4_2 = new JLabel("Неймовірно");
-		l_slider_putanna4_2.setForeground(Color.BLACK);
+		l_slider_putanna4_2.setForeground(Color.WHITE);
 		l_slider_putanna4_2.setFont(new Font("Times New Roman", Font.ITALIC, 18));
 		l_slider_putanna4_2.setBounds(693, 261, 101, 16);
 		getContentPane().add(l_slider_putanna4_2);
@@ -211,20 +220,20 @@ public class Kimnatne extends JFrame {
 		getContentPane().add(slider_putanna5);
 
 		l_slider_putanna5_1 = new JLabel("Раз на день");
-		l_slider_putanna5_1.setForeground(Color.BLACK);
+		l_slider_putanna5_1.setForeground(Color.WHITE);
 		l_slider_putanna5_1.setFont(new Font("Times New Roman", Font.ITALIC, 18));
 		l_slider_putanna5_1.setBounds(525, 326, 94, 16);
 		getContentPane().add(l_slider_putanna5_1);
 
 		l_slider_putanna5_2 = new JLabel("Раз на 2 тижні");
 		l_slider_putanna5_2.setHorizontalAlignment(SwingConstants.RIGHT);
-		l_slider_putanna5_2.setForeground(Color.BLACK);
+		l_slider_putanna5_2.setForeground(Color.WHITE);
 		l_slider_putanna5_2.setFont(new Font("Times New Roman", Font.ITALIC, 18));
 		l_slider_putanna5_2.setBounds(653, 326, 133, 16);
 		getContentPane().add(l_slider_putanna5_2);
 
 		radioB_putanna6_1 = new JRadioButton("Так");
-		radioB_putanna6_1.setForeground(Color.BLACK);
+		radioB_putanna6_1.setForeground(Color.WHITE);
 		radioB_putanna6_1.setOpaque(false);
 		radioB_putanna6_1.setFont(new Font("Times New Roman", Font.ITALIC, 18));
 		radioB_putanna6_1.setBounds(561, 368, 62, 25);
@@ -236,7 +245,7 @@ public class Kimnatne extends JFrame {
 		});
 
 		radioB_putanna6_2 = new JRadioButton("Ні");
-		radioB_putanna6_2.setForeground(Color.BLACK);
+		radioB_putanna6_2.setForeground(Color.WHITE);
 		radioB_putanna6_2.setOpaque(false);
 		radioB_putanna6_2.setFont(new Font("Times New Roman", Font.ITALIC, 18));
 		radioB_putanna6_2.setBounds(660, 368, 55, 25);
@@ -272,12 +281,7 @@ public class Kimnatne extends JFrame {
 		b_Dali.setBounds(89, 524, 606, 41);
 		getContentPane().add(b_Dali);
 
-//		l_fonVodaOputuvanna = new JLabel("");
-//		l_fonVodaOputuvanna.setBounds(0, 0, 794, 565);
-//		l_fonVodaOputuvanna.setIcon(new ImageIcon("res/fon_VodaOputuvanna.jpg"));
-//		getContentPane().add(l_fonVodaOputuvanna);
-		
-		JButton b_putanna2 = new JButton("Що це ?");
+		b_putanna2 = new JButton("Що це ?");
 		b_putanna2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				JOptionPane.showMessageDialog(null, 
@@ -291,6 +295,7 @@ public class Kimnatne extends JFrame {
 		getContentPane().add(b_putanna2);
 		
 		radioB_putanna3_1 = new JRadioButton("Так");
+		radioB_putanna3_1.setForeground(Color.WHITE);
 		radioB_putanna3_1.setOpaque(false);
 		radioB_putanna3_1.setFont(new Font("Times New Roman", Font.ITALIC, 18));
 		radioB_putanna3_1.setBounds(561, 179, 83, 25);
@@ -302,6 +307,7 @@ public class Kimnatne extends JFrame {
 		});
 		
 		radioB_putanna3_2 = new JRadioButton("Ні");
+		radioB_putanna3_2.setForeground(Color.WHITE);
 		radioB_putanna3_2.setOpaque(false);
 		radioB_putanna3_2.setFont(new Font("Times New Roman", Font.ITALIC, 18));
 		radioB_putanna3_2.setBounds(672, 179, 83, 25);
@@ -314,12 +320,13 @@ public class Kimnatne extends JFrame {
 		
 		l_slider_putanna5_3 = new JLabel("і більше");
 		l_slider_putanna5_3.setHorizontalAlignment(SwingConstants.RIGHT);
-		l_slider_putanna5_3.setForeground(Color.BLACK);
+		l_slider_putanna5_3.setForeground(Color.WHITE);
 		l_slider_putanna5_3.setFont(new Font("Times New Roman", Font.ITALIC, 18));
 		l_slider_putanna5_3.setBounds(653, 346, 134, 16);
 		getContentPane().add(l_slider_putanna5_3);
 		
 		l_putanna8 = new JLabel("Вас цікаве розмноження кімнатних рослин ?");
+		l_putanna8.setForeground(Color.WHITE);
 		l_putanna8.setHorizontalAlignment(SwingConstants.CENTER);
 		l_putanna8.setFont(new Font("Times New Roman", Font.BOLD, 20));
 		l_putanna8.setBounds(8, 472, 501, 30);
@@ -327,7 +334,7 @@ public class Kimnatne extends JFrame {
 		
 		radioB_putanna7_1 = new JRadioButton("Так");
 		radioB_putanna7_1.setOpaque(false);
-		radioB_putanna7_1.setForeground(Color.BLACK);
+		radioB_putanna7_1.setForeground(Color.WHITE);
 		radioB_putanna7_1.setFont(new Font("Times New Roman", Font.ITALIC, 18));
 		radioB_putanna7_1.setBounds(561, 421, 62, 25);
 		getContentPane().add(radioB_putanna7_1);
@@ -339,7 +346,7 @@ public class Kimnatne extends JFrame {
 		
 		radioB_putanna7_2 = new JRadioButton("Ні");
 		radioB_putanna7_2.setOpaque(false);
-		radioB_putanna7_2.setForeground(Color.BLACK);
+		radioB_putanna7_2.setForeground(Color.WHITE);
 		radioB_putanna7_2.setFont(new Font("Times New Roman", Font.ITALIC, 18));
 		radioB_putanna7_2.setBounds(660, 421, 55, 25);
 		getContentPane().add(radioB_putanna7_2);
@@ -351,7 +358,7 @@ public class Kimnatne extends JFrame {
 		
 		radioB_putanna8_1 = new JRadioButton("Так");
 		radioB_putanna8_1.setOpaque(false);
-		radioB_putanna8_1.setForeground(Color.BLACK);
+		radioB_putanna8_1.setForeground(Color.WHITE);
 		radioB_putanna8_1.setFont(new Font("Times New Roman", Font.ITALIC, 18));
 		radioB_putanna8_1.setBounds(561, 477, 62, 25);
 		getContentPane().add(radioB_putanna8_1);
@@ -363,7 +370,7 @@ public class Kimnatne extends JFrame {
 		
 		radioB_putanna8_2 = new JRadioButton("Ні");
 		radioB_putanna8_2.setOpaque(false);
-		radioB_putanna8_2.setForeground(Color.BLACK);
+		radioB_putanna8_2.setForeground(Color.WHITE);
 		radioB_putanna8_2.setFont(new Font("Times New Roman", Font.ITALIC, 18));
 		radioB_putanna8_2.setBounds(660, 477, 55, 25);
 		getContentPane().add(radioB_putanna8_2);
@@ -400,6 +407,13 @@ public class Kimnatne extends JFrame {
 		slider_putanna4.addChangeListener(slider4);
 		slider_putanna5.addChangeListener(slider5);
 
+
+		l_fonVodaOputuvanna = new JLabel("");
+		l_fonVodaOputuvanna.setBounds(0, 0, 794, 565);
+		l_fonVodaOputuvanna.setIcon(new ImageIcon("res/fon_Kimnatne.jpg"));
+		getContentPane().add(l_fonVodaOputuvanna);
+		
+		
 		setVisible(true);
 	}
 
