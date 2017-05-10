75a4d087f8d598d4526816f421d0f19a808f7509
Zakk <tapac.zakusylo@gmail.com>
2017-05-10 18:15:04 +0300

    Кімнатне розслинництво, бонсай - закінчив (з дизайном)

--- RoslunneGospodarstvo/src/main/java/KP/RoslunneGospodarstvo/KimnatneResultatu.java
+++ RoslunneGospodarstvo/src/main/java/KP/RoslunneGospodarstvo/KimnatneResultatu.java
@@ -1,7 +1,6 @@
 package KP.RoslunneGospodarstvo;
 
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Font;
 import java.awt.Panel;
 import java.awt.event.ActionEvent;
@@ -23,17 +22,20 @@ public class KimnatneResultatu extends JFrame {
 
 	private JLabel l_Nazva;
 	private JButton b_Nazad;
-	
-	private boolean boolean_kartunka1 = true ;
-	private boolean boolean_kartunka2 = true ;
-	private boolean boolean_kartunka3 = true ;
-	private boolean boolean_kartunka4 = true ;
 
-	private JLabel l_kartunka1;
+	private boolean boolean_kartunka1 = true;
+	private boolean boolean_kartunka2 = true;
+	private boolean boolean_kartunka3 = true;
+	private boolean boolean_kartunka4 = true;
+
+	private boolean boolean_Bonsai_Kartunka1 = true;
+	private boolean boolean_Bonsai_Kartunka2 = true;
+
+	private JLabel l_ZumSad_kartunka1;
 	private Panel panel_ZumSad;
-	private JLabel l_kartunka2;
-	private JLabel l_kartunka3;
-	private JLabel l_kartunka4;
+	private JLabel l_ZumSad_kartunka2;
+	private JLabel l_ZumSad_kartunka3;
+	private JLabel l_ZumSad_kartunka4;
 	private JLabel l_ZumSad;
 	private JScrollPane scrollPane;
 	private Panel panel_Prukmetu;
@@ -44,6 +46,10 @@ public class KimnatneResultatu extends JFrame {
 	private Panel panel_Rozmnogenna;
 	private Panel panel_Doglad;
 	private JLabel l_fon;
+	private JLabel l_Bonsai_Kartunka1;
+	private JLabel l_Bonsai_Kartunka2;
+	private JLabel l_Bonsai;
+	private JScrollPane scrollPane_1;
 
 	public KimnatneResultatu(String string, int i_putanna1, int i_putanna2, int i_putanna3, int i_slider_putanna4,
 			int i_slider_putanna5, int i_putanna6, int i_putanna7, int i_putanna8, int i_checkBox1_0, int i_checkBox1_1,
@@ -54,7 +60,7 @@ public class KimnatneResultatu extends JFrame {
 			int i_checkBox3_4, int i_checkBox3_5, int i_checkBox4_0, int i_checkBox4_1, int i_checkBox4_2,
 			int i_checkBox4_3, int i_checkBox4_4, int i_checkBox4_5, int i_checkBox4_6, int i_checkBox4_7,
 			int i_checkBox4_8, int i_checkBox4_9, int i_checkBox4_10) {
-	
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(800, 600);
 		setResizable(false);
@@ -78,134 +84,186 @@ public class KimnatneResultatu extends JFrame {
 		});
 		getContentPane().setLayout(null);
 		getContentPane().add(b_Nazad);
-		
+
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane.setBounds(12, 63, 770, 489);
 		getContentPane().add(tabbedPane);
-		
-		panel_ZumSad = new Panel();
-		tabbedPane.addTab("Зимовий сад / Оранжерея", null, panel_ZumSad, null);
-		panel_ZumSad.setLayout(null);
-		
-		l_kartunka1 = new JLabel("");		
-		l_kartunka1.addMouseListener(new MouseAdapter() {
-			@Override			
-			public void mouseClicked(MouseEvent arg0) {
-				if (boolean_kartunka1 == true){
-					l_kartunka1.setBounds(0, 0, 542, 459);
-					l_kartunka1.setIcon(new ImageIcon("res/ZumSad1.1.jpg"));
-					boolean_kartunka1 = false ; 
-				}else{
-					l_kartunka1.setBounds(0, 292, 187, 167);
-					l_kartunka1.setIcon(new ImageIcon("res/ZumSad1.jpg"));
-					boolean_kartunka1 = true ;
-				}				
-			}
-		});
-		l_kartunka1.setIcon(new ImageIcon("res/ZumSad1.jpg"));
-		l_kartunka1.setBounds(0, 292, 187, 167);
-		panel_ZumSad.add(l_kartunka1);
-		
-		l_kartunka2 = new JLabel("");
-		l_kartunka2.addMouseListener(new MouseAdapter() {
-			@Override			
-			public void mouseClicked(MouseEvent arg0) {
-				if (boolean_kartunka2 == true){
-					l_kartunka2.setBounds(0, 0, 640, 459);
-					l_kartunka1.setBounds(0, 0, 0, 0);
-					l_kartunka2.setIcon(new ImageIcon("res/ZumSad2.1.jpg"));
-					boolean_kartunka2 = false ; 
-				}else{
-					l_kartunka2.setBounds(188, 292, 223, 167);
-					l_kartunka1.setBounds(0, 292, 187, 167);
-					l_kartunka2.setIcon(new ImageIcon("res/ZumSad2.jpg"));
-					boolean_kartunka2 = true ;
-				}				
-			}
-		});
-		l_kartunka2.setIcon(new ImageIcon("res/ZumSad2.jpg"));
-		l_kartunka2.setBounds(188, 292, 223, 167);
-		panel_ZumSad.add(l_kartunka2);
-		
-		l_kartunka3 = new JLabel("");
-		l_kartunka3.addMouseListener(new MouseAdapter() {
-			@Override			
-			public void mouseClicked(MouseEvent arg0) {
-				if (boolean_kartunka3 == true){
-					l_kartunka3.setBounds(0, 0, 640, 459);
-					l_kartunka2.setBounds(0, 0, 0, 0);
-					l_kartunka1.setBounds(0, 0, 0, 0);
-					l_kartunka3.setIcon(new ImageIcon("res/ZumSad3.1.jpg"));
-					boolean_kartunka3 = false ; 
-				}else{
-					l_kartunka3.setBounds(412, 292, 223, 167);
-					l_kartunka1.setBounds(0, 292, 187, 167);
-					l_kartunka2.setBounds(188, 292, 223, 167);
-					l_kartunka3.setIcon(new ImageIcon("res/ZumSad3.jpg"));
-					boolean_kartunka3 = true ;
-				}				
-			}
-		});
-		l_kartunka3.setIcon(new ImageIcon("res/ZumSad3.jpg"));
-		l_kartunka3.setBounds(412, 292, 223, 167);
-		panel_ZumSad.add(l_kartunka3);
-		
-		l_kartunka4 = new JLabel("");
-		l_kartunka4.addMouseListener(new MouseAdapter() {
-			@Override			
-			public void mouseClicked(MouseEvent arg0) {
-				if (boolean_kartunka4 == true){
-					l_kartunka4.setBounds(408, 0, 360, 459);
-					l_kartunka3.setBounds(0, 0, 0, 0);
-					l_kartunka4.setIcon(new ImageIcon("res/ZumSad4.1.jpg"));
-					boolean_kartunka4 = false ; 
-				}else{
-					l_kartunka4.setBounds(637, 292, 131, 167);
-					l_kartunka3.setBounds(412, 292, 223, 167);
-					l_kartunka4.setIcon(new ImageIcon("res/ZumSad4.jpg"));
-					boolean_kartunka4 = true ;
-				}				
-			}
-		});
-		l_kartunka4.setIcon(new ImageIcon("res/ZumSad4.jpg"));
-		l_kartunka4.setBounds(637, 292, 131, 167);
-		panel_ZumSad.add(l_kartunka4);
-		
-		scrollPane = new JScrollPane();
-		scrollPane.setBounds(0, 0, 765, 291);
-		panel_ZumSad.add(scrollPane);
-		
-		l_ZumSad = new JLabel("");
-		scrollPane.setViewportView(l_ZumSad);
-		l_ZumSad.setIcon(new ImageIcon("res/ZumSad.PNG"));
-		
-		panel_Bonsai = new Panel();
-		tabbedPane.addTab("Бонсай / суісекі", null, panel_Bonsai, null);
-		
+
+		if (i_putanna1 == 1) {
+			panel_ZumSad = new Panel();
+			tabbedPane.addTab("Зимовий сад / Оранжерея", null, panel_ZumSad, null);
+			panel_ZumSad.setLayout(null);
+
+			l_ZumSad_kartunka1 = new JLabel("");
+			l_ZumSad_kartunka1.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent arg0) {
+					if (boolean_kartunka1 == true) {
+						l_ZumSad_kartunka1.setBounds(0, 0, 542, 459);
+						l_ZumSad_kartunka1.setIcon(new ImageIcon("res/ZumSad1.1.jpg"));
+						boolean_kartunka1 = false;
+					} else {
+						l_ZumSad_kartunka1.setBounds(0, 292, 187, 167);
+						l_ZumSad_kartunka1.setIcon(new ImageIcon("res/ZumSad1.jpg"));
+						boolean_kartunka1 = true;
+					}
+				}
+			});
+			l_ZumSad_kartunka1.setIcon(new ImageIcon("res/ZumSad1.jpg"));
+			l_ZumSad_kartunka1.setBounds(0, 292, 187, 167);
+			panel_ZumSad.add(l_ZumSad_kartunka1);
+
+			l_ZumSad_kartunka2 = new JLabel("");
+			l_ZumSad_kartunka2.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent arg0) {
+					if (boolean_kartunka2 == true) {
+						l_ZumSad_kartunka2.setBounds(0, 0, 640, 459);
+						l_ZumSad_kartunka1.setBounds(0, 0, 0, 0);
+						l_ZumSad_kartunka2.setIcon(new ImageIcon("res/ZumSad2.1.jpg"));
+						boolean_kartunka2 = false;
+					} else {
+						l_ZumSad_kartunka2.setBounds(188, 292, 223, 167);
+						l_ZumSad_kartunka1.setBounds(0, 292, 187, 167);
+						l_ZumSad_kartunka2.setIcon(new ImageIcon("res/ZumSad2.jpg"));
+						boolean_kartunka2 = true;
+					}
+				}
+			});
+			l_ZumSad_kartunka2.setIcon(new ImageIcon("res/ZumSad2.jpg"));
+			l_ZumSad_kartunka2.setBounds(188, 292, 223, 167);
+			panel_ZumSad.add(l_ZumSad_kartunka2);
+
+			l_ZumSad_kartunka3 = new JLabel("");
+			l_ZumSad_kartunka3.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent arg0) {
+					if (boolean_kartunka3 == true) {
+						l_ZumSad_kartunka3.setBounds(0, 0, 640, 459);
+						l_ZumSad_kartunka2.setBounds(0, 0, 0, 0);
+						l_ZumSad_kartunka1.setBounds(0, 0, 0, 0);
+						l_ZumSad_kartunka3.setIcon(new ImageIcon("res/ZumSad3.1.jpg"));
+						boolean_kartunka3 = false;
+					} else {
+						l_ZumSad_kartunka3.setBounds(412, 292, 223, 167);
+						l_ZumSad_kartunka1.setBounds(0, 292, 187, 167);
+						l_ZumSad_kartunka2.setBounds(188, 292, 223, 167);
+						l_ZumSad_kartunka3.setIcon(new ImageIcon("res/ZumSad3.jpg"));
+						boolean_kartunka3 = true;
+					}
+				}
+			});
+			l_ZumSad_kartunka3.setIcon(new ImageIcon("res/ZumSad3.jpg"));
+			l_ZumSad_kartunka3.setBounds(412, 292, 223, 167);
+			panel_ZumSad.add(l_ZumSad_kartunka3);
+
+			l_ZumSad_kartunka4 = new JLabel("");
+			l_ZumSad_kartunka4.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent arg0) {
+					if (boolean_kartunka4 == true) {
+						l_ZumSad_kartunka4.setBounds(408, 0, 360, 459);
+						l_ZumSad_kartunka3.setBounds(0, 0, 0, 0);
+						l_ZumSad_kartunka4.setIcon(new ImageIcon("res/ZumSad4.1.jpg"));
+						boolean_kartunka4 = false;
+					} else {
+						l_ZumSad_kartunka4.setBounds(637, 292, 131, 167);
+						l_ZumSad_kartunka3.setBounds(412, 292, 223, 167);
+						l_ZumSad_kartunka4.setIcon(new ImageIcon("res/ZumSad4.jpg"));
+						boolean_kartunka4 = true;
+					}
+				}
+			});
+			l_ZumSad_kartunka4.setIcon(new ImageIcon("res/ZumSad4.jpg"));
+			l_ZumSad_kartunka4.setBounds(637, 292, 131, 167);
+			panel_ZumSad.add(l_ZumSad_kartunka4);
+
+			scrollPane = new JScrollPane();
+			scrollPane.setBounds(0, 0, 765, 291);
+			panel_ZumSad.add(scrollPane);
+
+			l_ZumSad = new JLabel("");
+			scrollPane.setViewportView(l_ZumSad);
+			l_ZumSad.setIcon(new ImageIcon("res/ZumSad.PNG"));
+		}
+		if (i_putanna2 == 1) {
+			panel_Bonsai = new Panel();
+			tabbedPane.addTab("Бонсай / суісекі", null, panel_Bonsai, null);
+			panel_Bonsai.setLayout(null);
+
+			l_Bonsai_Kartunka1 = new JLabel("");
+			l_Bonsai_Kartunka1.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent arg0) {
+					if (boolean_Bonsai_Kartunka1 == true) {
+						l_Bonsai_Kartunka1.setBounds(420, 0, 345, 459);
+						l_Bonsai_Kartunka1.setIcon(new ImageIcon("res/Bonsai1.1.jpg"));
+						boolean_Bonsai_Kartunka1 = false;
+					} else {
+						l_Bonsai_Kartunka1.setBounds(555, 0, 210, 230);
+						l_Bonsai_Kartunka1.setIcon(new ImageIcon("res/Bonsai1.jpg"));
+						boolean_Bonsai_Kartunka1 = true;
+					}
+				}
+			});
+			l_Bonsai_Kartunka1.setBounds(555, 0, 210, 230);
+			l_Bonsai_Kartunka1.setIcon(new ImageIcon("res/Bonsai1.jpg"));
+			panel_Bonsai.add(l_Bonsai_Kartunka1);
+
+			l_Bonsai_Kartunka2 = new JLabel("");
+			l_Bonsai_Kartunka2.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent arg0) {
+					if (boolean_Bonsai_Kartunka2 == true) {
+						l_Bonsai_Kartunka2.setBounds(420, 0, 345, 459);
+						l_Bonsai_Kartunka1.setBounds(0, 0, 0, 0);
+						l_Bonsai_Kartunka2.setIcon(new ImageIcon("res/Bonsai2.1.jpg"));
+						boolean_Bonsai_Kartunka2 = false;
+					} else {
+						l_Bonsai_Kartunka2.setBounds(555, 229, 210, 230);
+						l_Bonsai_Kartunka1.setBounds(555, 0, 210, 230);
+						l_Bonsai_Kartunka2.setIcon(new ImageIcon("res/Bonsai2.jpg"));
+						boolean_Bonsai_Kartunka2 = true;
+					}
+				}
+			});
+			l_Bonsai_Kartunka2.setBounds(555, 229, 210, 230);
+			l_Bonsai_Kartunka2.setIcon(new ImageIcon("res/Bonsai2.jpg"));
+			panel_Bonsai.add(l_Bonsai_Kartunka2);
+
+			scrollPane_1 = new JScrollPane();
+			scrollPane_1.setBounds(0, 0, 553, 459);
+			panel_Bonsai.add(scrollPane_1);
+
+			l_Bonsai = new JLabel("");
+			scrollPane_1.setViewportView(l_Bonsai);
+			l_Bonsai.setIcon(new ImageIcon("res/Bonsai.PNG"));
+		}
+
 		panel_Prukmetu = new Panel();
 		tabbedPane.addTab("Прикмети", null, panel_Prukmetu, null);
-		
+
 		panel_Hkidnuku = new Panel();
 		tabbedPane.addTab("Шкідники", null, panel_Hkidnuku, null);
-		
+
 		panel_Hvorobu = new Panel();
 		tabbedPane.addTab("Хвороби", null, panel_Hvorobu, null);
-		
+
 		panel_Rozmnogenna = new Panel();
 		tabbedPane.addTab("Розмноження", null, panel_Rozmnogenna, null);
-		
+
 		panel_Kupivla = new Panel();
 		tabbedPane.addTab("Купівля", null, panel_Kupivla, null);
-		
+
 		panel_Doglad = new Panel();
 		tabbedPane.addTab("Догляд", null, panel_Doglad, null);
-		
-		 l_fon = new JLabel("");
-		 l_fon.setBounds(0, 0, 794, 565);
-		 l_fon.setIcon(new ImageIcon("res/fon_KimnatneResultat.jpg"));
-		 getContentPane().add(l_fon);
+
+		l_fon = new JLabel("");
+		l_fon.setBounds(0, 0, 794, 565);
+		l_fon.setIcon(new ImageIcon("res/fon_KimnatneResultat.jpg"));
+		getContentPane().add(l_fon);
 
 		setVisible(true);
-		
+
 	}
 }
